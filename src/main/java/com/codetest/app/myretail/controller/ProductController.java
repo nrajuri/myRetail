6dcfc88316ef8cf48444c106b772dbe7f66d598c
nrajuri <nrajuri@gmail.com>
2019-09-19 21:24:42 -0500

    javaDoc comments

--- src/main/java/com/codetest/app/myretail/controller/ProductController.java
+++ src/main/java/com/codetest/app/myretail/controller/ProductController.java
@@ -36,6 +36,13 @@ public class ProductController {
         return productService.getAllProducts();
     }
 
+    /**
+     * Gets product details by Id.
+     *
+     * @param productId
+     * @return ProductDto
+     * @throws MyRetailException
+     */
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<ProductDto> getProductById(@PathVariable("id") String productId) throws MyRetailException {
@@ -46,17 +53,18 @@ public class ProductController {
             return new ResponseEntity<ProductDto>(product, HttpStatus.OK);
         } catch (MyRetailException e) {
             log.error("Exception occurred while fetching product data.", e.getMessage());
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            throw new MyRetailException(e.getErrorCode(), e.getErrorMessage());
         }
     }
 
-    @ExceptionHandler(MyRetailException.class)
-    public ResponseEntity<ProductResponse> exceptionHandler(MyRetailException ex) {
-        ProductResponse error = new ProductResponse(ex.getErrorCode(), ex.getMessage());
-        log.debug(ex.getErrorMessage(), ex);
-        return new ResponseEntity<>(error, HttpStatus.valueOf(ex.getErrorCode()));
-    }
-
+    /**
+     * Updates product price details.
+     *
+     * @param product
+     * @param productId
+     * @return Ok status code.
+     * @throws MyRetailException
+     */
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ProductResponse> updatePrice(@RequestBody ProductDto product,
                                                        @PathVariable("id") String productId) throws MyRetailException {
@@ -65,5 +73,16 @@ public class ProductController {
         }
         productService.updateProductById(product);
         return new ResponseEntity<ProductResponse>(HttpStatus.OK);
+    }
+
+    /**
+     * @param ex
+     * @return error response
+     */
+    @ExceptionHandler(MyRetailException.class)
+    public ResponseEntity<ProductResponse> exceptionHandler(MyRetailException ex) {
+        ProductResponse error = new ProductResponse(ex.getErrorCode(), ex.getMessage());
+        log.debug(ex.getErrorMessage(), ex);
+        return new ResponseEntity<>(error, HttpStatus.valueOf(ex.getErrorCode()));
     }
 }
