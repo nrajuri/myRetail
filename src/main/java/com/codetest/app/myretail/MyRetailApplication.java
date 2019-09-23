dee2eb9cd0600d9482df577f7159996500b6d878
nrajuri <nrajuri@gmail.com>
2019-09-23 10:36:19 -0500

    clean up

--- src/main/java/com/codetest/app/myretail/MyRetailApplication.java
+++ src/main/java/com/codetest/app/myretail/MyRetailApplication.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
  * MyRetailApplication
  */
 @SpringBootApplication
-@Configuration
 @EnableCircuitBreaker
 @EnableHystrixDashboard
 public class MyRetailApplication {
